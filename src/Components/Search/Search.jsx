be4981b63e57318a8297ab67302b37f3b81793af
shubhamanurag22 <anuragshubham799@gmail.com>
2025-03-24 15:20:25 +0530

    added search feature

--- src/Components/Search/Search.jsx
+++ src/Components/Search/Search.jsx
@@ -1,9 +1,17 @@
+import useDebounce from '../../hooks/useDebounce';
 import './Search.css';
 
-function Search(){
+function Search( {updateSearchTerm} ){
+    const debounceCallback = useDebounce((e) => updateSearchTerm(e.target.value));
+
     return(
         <div className='search-wrapper'>
-            <input id="pokemon-name-search" type="text" placeholder="Pokemon Name..."/>
+            <input 
+                id="pokemon-name-search" 
+                type="text" 
+                placeholder="Pokemon Name..."
+                onChange={debounceCallback}
+            />
         </div>
     )
 }
